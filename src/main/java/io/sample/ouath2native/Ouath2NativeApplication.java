583c59c37cea0eaca458dfef7a9fc6dd5db672d7
issiscv <issiscv46@gmail.com>
2022-11-16 00:43:23 +0900

    git init

---
+++ src/main/java/io/sample/ouath2native/Ouath2NativeApplication.java
@@ -0,0 +1,13 @@
+package io.sample.ouath2native;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ouath2NativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ouath2NativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sample/ouath2native/Ouath2NativeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sample.ouath2native;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ouath2NativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
