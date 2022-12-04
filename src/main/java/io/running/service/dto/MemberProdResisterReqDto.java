1f8a3b958b0b4c7b68f34beda51990998713c9cc
Longstick0 <789456jang@naver.com>
2022-12-05 03:56:25 +0900

    fix: 병짓 수정;ㄷㄷ

--- src/main/java/io/running/service/dto/MemberProdResisterReqDto.java
+++ src/main/java/io/running/service/dto/MemberProdResisterReqDto.java
@@ -1,15 +1,18 @@
 package io.running.service.dto;
 
 import io.running.dto.req.MemberRegisterReqDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberProdResisterReqDto {
 
     private String nickname;
 
     private String introduce;
-
 
     public MemberProdResisterReqDto(MemberRegisterReqDto memberLocalRegisterReqDto) {
         this.nickname = memberLocalRegisterReqDto.getNickname();
